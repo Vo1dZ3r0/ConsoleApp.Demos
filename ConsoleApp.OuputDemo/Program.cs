1a2641a385e0ac5a672c67093f962376429d1ff1
Sir Void <vo1dz3r0@protonmail.com>
2024-08-18 18:36:50 -0500

    Updated project name and removed top level comment.

--- ConsoleApp.OuputDemo/Program.cs
+++ ConsoleApp.OuputDemo/Program.cs
@@ -1,4 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! My name is Bralin Larence");
+﻿Console.WriteLine("Hello, World! My name is Bralin Larence");
 
 Console.WriteLine("I am from the United States of America!");
