70f23c23606ca25326835af0f76dfdf31a60c63e
Sir Void <vo1dz3r0@protonmail.com>
2024-08-18 18:25:57 -0500

    Add project files.

---
+++ ConsoleApp.OuputDemo/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! My name is Bralin Larence");
+
+Console.WriteLine("I am from the United States of America!");
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Different datatypes
+/*
+    text - string
+    integers -int
+    decimal - double, float, decimal
+    logical - bool
+*/
+
+string name = "Bralin Larence";
+Console.WriteLine(name);
+Console.WriteLine("I am " + name); //  string concatenation 
+Console.WriteLine($"They call me {name}"); // string interpolation
+Console.WriteLine("I was given the name {0}", name); // formatted string
+
+int age = 60;
+Console.WriteLine("\nMy age is: " + age);
+int retirementYearsLeft = 5;
+int retirementAge = age + retirementYearsLeft;
+Console.WriteLine($"I will retire at age {retirementAge}");
+
+bool isRetired = false;
+
+Console.WriteLine("Am I retired? " + isRetired);
+
