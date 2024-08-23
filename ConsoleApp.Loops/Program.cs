72b88cfad63924a81663c1d0e64d646258b1dd2c
Sir Void <vo1dz3r0@protonmail.com>
2024-08-23 00:35:09 -0500

    Added loop demos and modified calculator.

---
+++ ConsoleApp.Loops/Program.cs
@@ -0,0 +1,66 @@
+﻿// Simple For Loop Demo
+//Print Hello World 10 times 
+
+// Ask the User how many times they wish to print hello world and print accordingly
+
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("How many times would you like to print Hello World");
+int numberOfPrints = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("**************** Simple For Loop ***************************");
+for (int i = 0; i < numberOfPrints; ++i)
+{
+    Console.WriteLine($"hello, world!!! - {i + 1}");
+}
+Console.WriteLine("**************** Simple For Loop Ended ***************************\n");
+
+
+Console.WriteLine("*********************** While Loop ***************************");
+
+int counter = 0; 
+while(counter < numberOfPrints)
+{
+    Console.WriteLine($"Hello, World!!! - {counter + 1}");
+    counter++;
+
+}
+
+// Ask the user for a number and find the total for each number that is entered. Print the final sum when the user
+// enters -1 to exit.
+int sum = 0;
+int input = 0;
+Console.WriteLine("\nPlease enter a number to be summed up. (press -1 to stop or exit)");
+while(input != -1)
+{
+    input = Convert.ToInt32(Console.ReadLine());
+    if(input != -1)
+    {
+        sum += input;
+    }
+    
+    
+}
+
+Console.WriteLine($"Your sum is {sum}");
+
+Console.WriteLine("********************* While Loop Ended ***************************\n");
+
+Console.WriteLine("*********************** Do While Loop ***************************");
+sum = 0;
+input = 0;
+do
+{
+    Console.WriteLine("\nPlease enter a number to be summed up. (press -1 to stop or exit)");
+    input = Convert.ToInt32(Console.ReadLine());
+    if (input != -1)
+    {
+        sum += input;
+    }
+
+} while (input != -1);
+
+Console.WriteLine($"Your sum is {sum}");
+
+Console.WriteLine("*********************** Do While Loop Ended ***************************");
+
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,48 +1,80 @@
 ﻿// Welcome message
+using System.ComponentModel.DataAnnotations;
+
 Console.WriteLine("Welcome to the sample calculator!");
 
-// Prompt for user input
-Console.WriteLine("Please enter the first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Please enter the second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
 
 // Show calculator options
-Console.WriteLine("Please select the operation you would like to perform");
+Console.WriteLine("\nPlease select the operation you would like to perform (q to exit)");
 Console.Write("(+) - Addition\n");
 Console.Write("(-) - Subtraction\n");
 Console.Write("(*) - Multiplication\n");
 Console.Write("(/) - Division\n");
-Console.Write("(%) - Modulus\n\n");
-char operation = Convert.ToChar(Console.ReadLine());
+Console.Write("(%) - Modulus\n");
+Console.Write("(f) - Fibonacci Sequence\n\n");
+char input = Convert.ToChar(Console.ReadLine());
 Console.Write("\n");
 
-Console.WriteLine($"You've chosen {operation}.");
-
-// Decide which operation is needed based on selected option 
-int result = 0;
-switch (operation)
+while (input != 'q')
 {
-    case '+':
-        result = num1 + num2;
-        break;
-    case '-':
-        result = num1 - num2;
-        break;
-    case '*':
-        result = num1 * num2;
-        break;
-    case '/':
-        result = num1 / num2;
-        break;
-    case '%':
-        result = num1 % num2;
-        break;
-    default:
-        Console.WriteLine("Invalid choice");
-        break;
-}
-//print output
+    
 
-Console.WriteLine($"The result is {result}.");
+    Console.WriteLine($"You've chosen {input}.\n");
+    // Prompt for user input
+    Console.WriteLine("Please enter the first number: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Please enter the second number: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    // Decide which operation is needed based on selected option 
+    int result = 0;
+    switch (input)
+    {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        case '%':
+            result = num1 % num2;
+            break;
+        case 'f':
+            for (int i = num1; i <= num2; i++)
+            {
+                result += i;
+            }
+            break;
+        default:
+            Console.WriteLine("Invalid choice");
+            break;
+    }
+    //print output
+    Console.WriteLine($"The result is {result}.\n");
+    Console.WriteLine("Press Enter to continue.");
+    Console.ReadLine();
+    Console.Clear();
+
+    // Show calculator options
+    Console.WriteLine("\nPlease select the operation you would like to perform (q to exit)");
+    Console.Write("(+) - Addition\n");
+    Console.Write("(-) - Subtraction\n");
+    Console.Write("(*) - Multiplication\n");
+    Console.Write("(/) - Division\n");
+    Console.Write("(%) - Modulus\n");
+    Console.Write("(f) - Fibonacci Sequence\n\n");
+    input = Convert.ToChar(Console.ReadLine());
+    Console.Write("\n");
+    
+
+    
+} 
+
+Console.WriteLine("Thank you for using the sample calculator!!!!");
